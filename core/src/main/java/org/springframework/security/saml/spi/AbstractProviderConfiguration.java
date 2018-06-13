13535bcec9c66a6e045fd3bfa6d3bc0d9e16eb32
Filip Hanik <fhanik@pivotal.io>
2018-06-13 10:49:36 -0700

    Refactor configuration into core library

--- core/src/main/java/org/springframework/security/saml/spi/AbstractProviderConfiguration.java
+++ core/src/main/java/org/springframework/security/saml/spi/AbstractProviderConfiguration.java
@@ -13,37 +13,29 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  *
-*/
-package sample.config;
+ */
+
+package org.springframework.security.saml.spi;
 
 import java.time.Clock;
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.security.saml.SamlMessageHandler;
 import org.springframework.security.saml.SamlObjectResolver;
 import org.springframework.security.saml.SamlProcessingFilter;
-import org.springframework.security.saml.SamlMessageHandler;
+import org.springframework.security.saml.SamlTemplateEngine;
 import org.springframework.security.saml.SamlTransformer;
 import org.springframework.security.saml.SamlValidator;
 import org.springframework.security.saml.config.SamlServerConfiguration;
-import org.springframework.security.saml.spi.DefaultAuthnRequestHandler;
-import org.springframework.security.saml.spi.DefaultLogoutHandler;
-import org.springframework.security.saml.spi.DefaultMetadataCache;
-import org.springframework.security.saml.spi.DefaultMetadataHandler;
-import org.springframework.security.saml.spi.DefaultSamlObjectResolver;
-import org.springframework.security.saml.spi.DefaultSamlTransformer;
-import org.springframework.security.saml.spi.DefaultSessionAssertionStore;
-import org.springframework.security.saml.spi.DefaultSpResponseHandler;
-import org.springframework.security.saml.spi.DefaultValidator;
-import org.springframework.security.saml.spi.Defaults;
-import org.springframework.security.saml.spi.SpringSecuritySaml;
 import org.springframework.security.saml.spi.opensaml.OpenSamlImplementation;
+import org.springframework.security.saml.spi.opensaml.OpenSamlVelocityEngine;
 import org.springframework.security.saml.util.Network;
 
-@Configuration
-public class SamlConfiguration {
+public abstract class AbstractProviderConfiguration {
+
+	@Bean
+	public abstract List<SamlMessageHandler> handlers(SamlServerConfiguration configuration);
 
 	@Bean
 	public SamlProcessingFilter samlFilter(List<SamlMessageHandler> handlers) {
@@ -52,18 +44,13 @@ public class SamlConfiguration {
 	}
 
 	@Bean
-	public List<SamlMessageHandler> handlers(SamlServerConfiguration configuration) {
-		return Arrays.asList(
-			metadataHandler(configuration),
-			discoveryHandler(configuration),
-			logoutHandler(configuration),
-			spResponseHandler(configuration)
-		);
-	}
-
-	@Bean
-	public DefaultSessionAssertionStore assertionStore() {
-		return new DefaultSessionAssertionStore();
+	public SamlMessageHandler metadataHandler(SamlServerConfiguration configuration) {
+		return new DefaultMetadataHandler()
+			.setDefaults(defaults())
+			.setNetwork(network())
+			.setResolver(resolver())
+			.setTransformer(transformer())
+			.setConfiguration(configuration);
 	}
 
 	@Bean
@@ -79,34 +66,14 @@ public class SamlConfiguration {
 	}
 
 	@Bean
-	public SamlMessageHandler metadataHandler(SamlServerConfiguration configuration) {
-		return new DefaultMetadataHandler()
-			.setDefaults(defaults())
-			.setNetwork(network())
-			.setResolver(resolver())
-			.setTransformer(transformer())
-			.setConfiguration(configuration);
+	public DefaultSessionAssertionStore assertionStore() {
+		return new DefaultSessionAssertionStore();
 	}
 
-	@Bean
-	public SamlMessageHandler discoveryHandler(SamlServerConfiguration configuration) {
-		return new DefaultAuthnRequestHandler()
-			.setDefaults(defaults())
-			.setNetwork(network())
-			.setResolver(resolver())
-			.setTransformer(transformer())
-			.setConfiguration(configuration);
-	}
 
 	@Bean
-	public SamlMessageHandler spResponseHandler(SamlServerConfiguration configuration) {
-		return new DefaultSpResponseHandler()
-			.setDefaults(defaults())
-			.setNetwork(network())
-			.setResolver(resolver())
-			.setTransformer(transformer())
-			.setConfiguration(configuration)
-			.setValidator(validator());
+	public SamlTemplateEngine samlTemplateEngine() {
+		return new OpenSamlVelocityEngine();
 	}
 
 	@Bean

5d00dbcc61cf291593c51d3c8ccc518a5ce0d704
Filip Hanik <fhanik@pivotal.io>
2018-09-10 12:39:32 -0700

    Even the thread local should expire

--- core/src/main/java/org/springframework/security/saml/provider/config/ThreadLocalSamlConfigurationRepository.java
+++ core/src/main/java/org/springframework/security/saml/provider/config/ThreadLocalSamlConfigurationRepository.java
@@ -1,21 +1,37 @@
 package org.springframework.security.saml.provider.config;
 
+import java.time.Clock;
+
 import org.springframework.security.saml.SamlException;
 import org.springframework.security.saml.provider.SamlServerConfiguration;
 
 public class ThreadLocalSamlConfigurationRepository implements SamlConfigurationRepository {
 
-	private static InheritableThreadLocal<SamlServerConfiguration> threadLocal = new InheritableThreadLocal<>();
+	private static InheritableThreadLocal<ExpiringEntry> threadLocal = new InheritableThreadLocal<>();
 
 	private final SamlConfigurationRepository initialValueProvider;
+	private final Clock clock;
+	private long expirationMillis = 10 * 1000;
 
 	public ThreadLocalSamlConfigurationRepository(SamlConfigurationRepository initialValueProvider) {
+		this(initialValueProvider, Clock.systemUTC());
+	}
+
+	public ThreadLocalSamlConfigurationRepository(SamlConfigurationRepository initialValueProvider, Clock clock) {
 		this.initialValueProvider = initialValueProvider;
+		this.clock = clock;
 	}
 
 	@Override
 	public SamlServerConfiguration getServerConfiguration() {
-		SamlServerConfiguration result = threadLocal.get();
+		ExpiringEntry expiringEntry = threadLocal.get();
+		SamlServerConfiguration result = null;
+		if (expiringEntry != null) {
+			result = expiringEntry.getConfiguration(getExpirationMillis());
+			if (result == null) {
+				reset();
+			}
+		}
 		if (result == null) {
 			try {
 				result = initialValueProvider.getServerConfiguration().clone();
@@ -26,11 +42,56 @@ public class ThreadLocalSamlConfigurationRepository implements SamlConfiguration
 		return result;
 	}
 
-	void setServerConfiguration(SamlServerConfiguration configuration) {
-		threadLocal.set(configuration);
+	protected void setServerConfiguration(SamlServerConfiguration configuration) {
+		if (configuration == null) {
+			reset();
+		}
+		else {
+			threadLocal.set(
+				new ExpiringEntry(clock, configuration)
+			);
+		}
 	}
 
 	public void reset() {
 		threadLocal.remove();
+	}
+
+	public long getExpirationMillis() {
+		return expirationMillis;
+	}
+
+	public ThreadLocalSamlConfigurationRepository setExpirationMillis(long expirationMillis) {
+		this.expirationMillis = expirationMillis;
+		return this;
+	}
+
+	private static class ExpiringEntry {
+		private Clock clock;
+		private long created;
+		private SamlServerConfiguration configuration;
+
+		public ExpiringEntry(Clock clock, SamlServerConfiguration configuration) {
+			this.clock = clock;
+			setConfiguration(configuration);
+		}
+
+		public long getCreated() {
+			return created;
+		}
+
+		public void setConfiguration(SamlServerConfiguration configuration) {
+			this.configuration = configuration;
+			created = configuration==null ? 0 : clock.millis();
+		}
+
+		public SamlServerConfiguration getConfiguration(long expiration) {
+			if ((created+expiration) > clock.millis()) {
+				return configuration;
+			}
+			else {
+				return null;
+			}
+		}
 	}
 }
---
+++ core/src/test/java/org/springframework/security/saml/provider/config/ThreadLocalSamlConfigurationRepositoryTests.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2002-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package org.springframework.security.saml.provider.config;
+
+import java.time.Clock;
+
+import org.springframework.security.saml.provider.SamlServerConfiguration;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.stubbing.Answer;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.when;
+
+class ThreadLocalSamlConfigurationRepositoryTests {
+
+
+	private ThreadLocalSamlConfigurationRepository repository;
+	private SamlServerConfiguration configuration;
+	private Clock clock;
+	private InheritableThreadLocal threadLocal;
+
+	@BeforeEach
+	public void setup() {
+		SamlConfigurationRepository mockConfig = mock(SamlConfigurationRepository.class);
+		clock = mock(Clock.class);
+		repository = new ThreadLocalSamlConfigurationRepository(mockConfig, clock);
+		configuration = new SamlServerConfiguration();
+		when(mockConfig.getServerConfiguration()).thenReturn(configuration);
+		when(clock.millis()).thenAnswer((Answer<Long>) invocation -> System.currentTimeMillis());
+		threadLocal = (InheritableThreadLocal) ReflectionTestUtils
+			.getField(ThreadLocalSamlConfigurationRepository.class, "threadLocal");
+	}
+
+	@AfterEach
+	public void breakdown() {
+		repository.setServerConfiguration(null);
+	}
+
+	@Test
+	public void get_creates_clone() {
+		assertNotSame(configuration, repository.getServerConfiguration());
+	}
+
+	@Test
+	public void configuration_doesnt_get_set() {
+		repository.getServerConfiguration();
+		assertNull(threadLocal.get());
+	}
+
+	@Test
+	public void cached_entry_is_returned() {
+		SamlServerConfiguration c1 = repository.getServerConfiguration();
+		repository.setServerConfiguration(c1);
+		assertSame(c1, repository.getServerConfiguration());
+	}
+
+	@Test
+	public void reset_works() {
+		SamlServerConfiguration c1 = repository.getServerConfiguration();
+		repository.setServerConfiguration(c1);
+		assertNotNull(threadLocal.get());
+		repository.reset();
+		assertNull(threadLocal.get());
+	}
+
+	@Test
+	public void set_null_works() {
+		SamlServerConfiguration c1 = repository.getServerConfiguration();
+		repository.setServerConfiguration(c1);
+		assertNotNull(threadLocal.get());
+		repository.setServerConfiguration(null);
+		assertNull(threadLocal.get());
+	}
+
+	@Test
+	public void expiration_works() {
+		SamlServerConfiguration c1 = repository.getServerConfiguration();
+		repository.setServerConfiguration(c1);
+		assertNotNull(threadLocal.get());
+
+		SamlServerConfiguration c2 = repository.getServerConfiguration();
+		assertSame(c1,c2);
+		reset(clock);
+		when(clock.millis()).thenReturn(System.currentTimeMillis() + (repository.getExpirationMillis()*2));
+
+		SamlServerConfiguration c3 = repository.getServerConfiguration();
+		assertNotSame(c2,c3);
+		assertNull(threadLocal.get());
+	}
+
+
+}

083029eda3e5a139a5fafe5d4e1d905b2e9202de
Filip Hanik <fhanik@pivotal.io>
2018-12-11 22:04:56 -0800

    Correct displayname of test class

--- samples/boot/boot-config-sp/src/test/java/org/springframework/security/samples/ServiceProviderMetadataTests.java
+++ samples/boot/boot-config-sp/src/test/java/org/springframework/security/samples/ServiceProviderMetadataTests.java
@@ -47,7 +47,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-@DisplayName("SAML Service Provider Single Logout")
+@DisplayName("SAML Service Provider Metadata")
 public class ServiceProviderMetadataTests extends AbstractServiceProviderTestBase {
 
 	@SpringBootConfiguration

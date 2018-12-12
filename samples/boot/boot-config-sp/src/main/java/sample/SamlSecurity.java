1f6134a8c29d26cd8575f8db85b45b289e1df566
Filip Hanik <fhanik@pivotal.io>
2018-12-11 18:38:06 -0800

    Remove the external LogoutFilter

--- samples/boot/boot-config-sp/src/main/java/sample/SamlSecurity.java
+++ samples/boot/boot-config-sp/src/main/java/sample/SamlSecurity.java
@@ -47,8 +47,6 @@ public class SamlSecurity extends WebSecurityConfigurerAdapter {
 				.authorizeRequests()
 				.anyRequest().authenticated()
 			.and()
-				.logout()
-			.and()
 				.apply(
 					saml2Login()
 				)

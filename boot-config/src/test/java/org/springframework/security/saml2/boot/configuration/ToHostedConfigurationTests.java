57ceddb47992cc4c846e0417b096654299cd24d8
Filip Hanik <fhanik@pivotal.io>
2019-04-23 11:22:05 -0700

    align packages

--- boot-config/src/test/java/org/springframework/security/saml2/boot/configuration/ToHostedConfigurationTests.java
+++ boot-config/src/test/java/org/springframework/security/saml2/boot/configuration/ToHostedConfigurationTests.java
@@ -5,29 +5,23 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     https://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
-package org.springframework.security.saml2.boot;
+package org.springframework.security.saml2.boot.configuration;
 
-import org.springframework.security.saml2.boot.configuration.LocalSaml2IdentityProviderConfiguration;
-import org.springframework.security.saml2.boot.configuration.LocalSaml2ProviderConfiguration;
-import org.springframework.security.saml2.boot.configuration.LocalSaml2ServiceProviderConfiguration;
-import org.springframework.security.saml2.boot.configuration.RemoteSaml2IdentityProviderConfiguration;
-import org.springframework.security.saml2.boot.configuration.RemoteSaml2ProviderConfiguration;
-import org.springframework.security.saml2.boot.configuration.RemoteSaml2ServiceProviderConfiguration;
+import org.springframework.security.saml2.registration.ExternalSaml2IdentityProviderRegistration;
 import org.springframework.security.saml2.registration.ExternalSaml2ProviderRegistration;
-import org.springframework.security.saml2.registration.HostedSaml2ProviderRegistration;
 import org.springframework.security.saml2.registration.ExternalSaml2ServiceProviderRegistration;
 import org.springframework.security.saml2.registration.HostedSaml2IdentityProviderRegistration;
-import org.springframework.security.saml2.registration.ExternalSaml2IdentityProviderRegistration;
+import org.springframework.security.saml2.registration.HostedSaml2ProviderRegistration;
 import org.springframework.security.saml2.registration.HostedSaml2ServiceProviderRegistration;
 
 import org.junit.jupiter.api.Test;
